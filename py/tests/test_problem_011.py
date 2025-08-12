a9ef3c61602565cc3351a5071e056323c9907182
breadnbuddr <117009437+breadnbuddr@users.noreply.github.com>
2025-08-13 00:43:22 +0200

    Add solution and test for problem 11

---
+++ py/tests/test_problem_011.py
@@ -0,0 +1,10 @@
+from euler.problem_011 import solve
+
+
+def test_problem_011():
+    """
+    Test for Problem for Problem 011: Largest product in a grid
+    """
+    assert (
+        solve() == 70600674
+    ), "Expected result for Problem 011: Largest product in a grid is 70600674"
